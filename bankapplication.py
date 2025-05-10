375cde34eed41a815f6d91ff8a98c3e82dbb9c80
Gunareththinam Mithurshanan <UT010240@unicomtic.com>
2025-05-10 13:16:51 +0530

    initial commit

---
+++ bankapplication.py
@@ -0,0 +1,63 @@
+  #Create  Account account={}
+   
+           
+def create_account() :
+    
+        user=input("Enter Your Username:")
+        if user in account:
+            print("Account Already Exist")
+        else:
+             password = input("enter your password:")
+              account [user]  =     {   "password":password,   "balance"   :0}
+              print("Account Has Been Successfully Created:")
+
+def login():
+    Print("LOGIN")
+    Name=Input("User Name:")
+    password=input("password:")
+    if name in accountand account[name] 
+    ["password"]    ==  password:
+       print("Log In Successful")
+    else:
+        print("Invalid User Name Or Password")
+def menu():
+    print("1.Check Balance")
+    print("2.Check Balance")
+    print("3.Deposit Money")
+    print("4.exit From The Menu")
+
+def start():
+    while true:
+    print("1.Log In")
+    print("2.Create an account")
+    print("3.Exit")
+    option=input("Choose Option:")
+     if option  ==  "1"
+      user=login():
+    if user:
+        while true:
+                  menu():
+                  choice=input("Enter Choice:")
+        if choice   ==  ("1")
+        print(balance:account("user") ("balance"))
+        elif choice==   ("2")
+        amount=float(input("Enter The Amount To Withdraw"))
+         account(user)("balance")
+         print("Money Withdrawn Successfully")
+        elif choice==   ("3")
+        amount=float(input("Enter The Amount To Deposit"))_
+        account(user)("balance")
+             print("Money Deposited Successfully")
+            print("Not Valid Amount")
+        elif choice==   ("4")
+            print ("Thank You For Banking With Us")
+        break
+        else:
+            print("Invalid Choice")
+            print("Exit From The Menu")
+
+
+
+    
+ 
+ 
